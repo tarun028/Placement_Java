dab01cb940cd66593eaad08db5586de604143dc4
tarun028 <tarunsrivastavakvs@gmail.com>
2024-06-30 18:44:03 +0530

    mistake

--- Placements/prefixsum.java
+++ Placements/prefixsum.java
@@ -1,5 +1,16 @@
+import java.util.*;
 public class prefixsum {
+
+    public static void input(int arr[],int n){
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            Scanner sc = new Scanner(System.in);
+            sc.nextInt();
+            System.out.println(a);
+        }
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        input(null,5);
     }
 }
