035f6eaf3fecb2482275ff0c35910cf42ca81232
tarun028 <tarunsrivastavakvs@gmail.com>
2024-07-04 22:35:03 +0530

    2d

---
+++ Placements/ddarray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ddarray {
+    public static void main(String[] args) {
+        System.err.println("2d ARRAY");
+
+        int matrix[][]= new int[3][4];
+        int n=3;
+        int m=4;
+
+        
+    }
+}
