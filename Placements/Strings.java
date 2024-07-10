55644b00715aa6527b4551ad3151ceb4214a45da
tarun028 <tarunsrivastavakvs@gmail.com>
2024-07-10 17:49:49 +0530

    strings

---
+++ Placements/Strings.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Strings {
+    public static void main(String[] args) {
+        //String name = "Tarun";
+        String fullName = "Tarun Srivastava";
+ 
+        Scanner sc = new Scanner(System.in);
+        //String name = sc.nextLine();
+        //System.out.println("name is = "+name);
+ 
+        //functions
+
+        String firstName = "Tarun";
+        String lastname = "Srivastava";
+        String mainName = firstName+" "+lastname;
+        System.out.println(mainName);
+    }
+}
--- Placements/ddarray.java
+++ Placements/ddarray.java
@@ -4,9 +4,21 @@ public class ddarray {
         System.err.println("2d ARRAY");
 
         int matrix[][]= new int[3][4];
-        int n=3;
-        int m=4;
+        int n=matrix.length;
+        int m=matrix[0].length;
 
+        Scanner sc = new Scanner(System.in);
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.println(matrix[i][j]);
+            }
+        }
         
     }
 }
