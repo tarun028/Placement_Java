bb35f6f42952cdb72e670ebc33150abbacd263cb
tarun028 <tarunsrivastavakvs@gmail.com>
2024-07-15 21:31:33 +0530

    recursion

---
+++ Placements/recursion.java
@@ -0,0 +1,33 @@
+public class recursion {
+
+    public static void printminus(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printminus(n-1);
+    }
+    public static void printplus(int n){
+        if(n==6){
+            return;
+        }
+        System.out.println(n);
+        printplus(n+1);
+    }
+
+    public static void sum(int i,int n,int sum){
+
+        if(i==n){
+            sum=sum+i;
+            System.out.println(sum);
+            return;
+        }
+        sum=sum+i;
+        sum(i+1,n,sum);
+    }
+    public static void main(String[] args) {
+        int n=1;
+        //printplus(n);
+        sum(0, 3, 0);
+    }
+}
